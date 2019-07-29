482f275e35a329379ed8345aab825d30616ff179
Eldiyar Talantbek <eldiyartalantbek@Eldiyars-MacBook-Pro.local>
2019-07-29 14:53:38 -0500

    use card

--- src/Items.js
+++ src/Items.js
@@ -1,22 +1,25 @@
 import React, {Component} from 'react'
 import Item from './Item'
+import Card from './common/Card'
 
 class Items extends Component {
   render() {
     const {items, checkOffItem, removeItem} = this.props
     return (
-      <div className="items-container">
-        {items.map(item => {
-          return (
-            <Item
-              item={item}
-              onCheckOff={checkOffItem}
-              onRemoveItem={removeItem}
-              key={item.id}
-            />
-          )
-        })}
-      </div>
+      <Card>
+        <div className="items-container">
+          {items.map(item => {
+            return (
+              <Item
+                item={item}
+                onCheckOff={checkOffItem}
+                onRemoveItem={removeItem}
+                key={item.id}
+              />
+            )
+          })}
+        </div>
+      </Card>
     )
   }
 }
